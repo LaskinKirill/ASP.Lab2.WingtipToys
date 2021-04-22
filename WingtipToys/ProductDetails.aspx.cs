343f5d30988e235745571edaa0227dd2b8cc0b10
Kirill Laskin <KirillLaskin@itcenterifmo.onmicrosoft.com>
2021-04-22 21:53:37 +0300

    Добавьте файлы проекта.

---
+++ WingtipToys/ProductDetails.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WingtipToys.Business;
+using WingtipToys.Data;
+using WingtipToys.Data.Models;
+
+namespace WingtipToys
+{
+   
+    public partial class ProductDetails : System.Web.UI.Page
+    {
+        private static readonly IStoreService _service = new StoreService(new InMemoryProductRepository(), new InMemoryCategoryRepository());
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public IQueryable<Product> GetProduct([RouteData("productId")] int? productId)
+        {
+            if (!productId.HasValue || productId.Value == 0)
+            {
+                return null;
+            }
+            return new List<Product>(1) { _service.GetProduct(productId.Value) }.AsQueryable();
+        }
+
+    }
+}
